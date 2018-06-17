21fddfcee0f7c62f4beedde1bbcca9ceaa41d10a
mrangius <angiusmarco@live.it>
2018-06-17 18:06:33 +0200

    fixed en dataset

--- translate.py
+++ translate.py
@@ -5,7 +5,7 @@ def _foo():
     with io.open("./jappo.csv", 'r', encoding="utf-8") as csvfile:
 
         recipes = csv.reader(csvfile, delimiter=';')
-        f = open("./jappo_translate.txt", "w")
+        f = open("./jappo_to_translate.txt", "w")
         for recipe in recipes:
             for ingredient in recipe:
                 f.write(ingredient.lower() + "\n")
@@ -13,13 +13,14 @@ def _foo():
 
         f.close()
 
+def _foo2():
+    with io.open("./jappo_translated.txt", "r") as f:
 
-if __name__ == '__main__':
-
-    with io.open("./jappo_translated.rtf", "r") as f:
-
-        line = f.readlines()[9:-1]
-        res = ",".join(line).replace("\\\n", "").replace(',####,', '\n')
+        line = f.readlines()
+        res = ",".join(line).replace("\n", "").replace(',####,', '\n')
 
         s = io.open("./jappo_final.csv", "w")
         s.write(res)
+
+if __name__ == '__main__':
+    _foo2()
